5d635de8e4b59959831ce58e73c48d2d88072c2f
Jvism <jvi.smo@gmail.com>
2021-10-22 16:01:28 -0300

    Creacion de archivo csv

---
+++ app/prueba.py
@@ -0,0 +1,6 @@
+from typing import ByteString
+import pandas as pd
+
+bd = pd.read_csv('./books_data.csv')
+
+print(bd)
--- app/scraping.py
+++ app/scraping.py
@@ -1,5 +1,12 @@
 import requests as req
 from bs4 import BeautifulSoup as bs
+import os
+
+def clearConsole():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+        command = 'cls'
+    os.system(command)
 
 def soup_recover(url):
     request = req.get(url)
@@ -9,19 +16,55 @@ def urls_extract(soup):
     
     urls = []
     for article in soup.find_all('article'):
-        for url in article.find_all('a',limit=1):
-            urls.append('https://books.toscrape.com/catalogue/' + url['href'])
+        url = article.find_all('a',limit=1)[0]
+        urls.append('https://books.toscrape.com/catalogue/' + url['href'])
 
     return urls
 
 def extract_data(soup):
 
     data_book = []
-    for article in soup.find_all('article'):
-        for url in article.find_all('a',limit=1):
-            urls.append('https://books.toscrape.com/catalogue/' + url['href'])
+
+    for page in soup.select('.page'):
+
+        book_information = page.find_all('td')
+
+
+        title = page.find_all('h1')[0].text
+        price = page.find_all('p')[0].text
+        stock = book_information[5].text
+        category = page.find_all('li')[2].text.split('\n')[1]
+        cover = 'https://books.toscrape.com/' + page.find_all('img')[0]['src'].split('../')[2]
+        upc = book_information[0].text
+        product_type = book_information[1].text
+        price_excl_tax = book_information[2].text
+        price_incl_tax = book_information[3].text
+        tax = book_information[4].text
+        number_reviews = book_information[6].text
+
+        print(book_information)
+
+        data_book.extend([title,price,stock,category,cover,upc,product_type,price_excl_tax,price_incl_tax,tax,stock,number_reviews])
 
     return data_book
+
+def export_csv(data):
+    contador = 0
+
+    file = open('books_data.csv','w',encoding="utf-8")
+    file.write('"title","price","stock","category","cover","upc","product type","price (excl. tax)","price (incl. tac)","tax","availability","number of reviews"\n')
+
+    for book_data in data:
+        info = ''
+        for information in book_data:
+            info += '"'+ information + '"' + ','
+        file.write(str(contador+1)+ ',' + info + '\n')
+        clearConsole()
+        print(str(round(contador*0.1,1)) + '%')
+
+        contador += 1
+
+    file.close()
 
 url_web = 'https://books.toscrape.com/'
 urls_books = []
@@ -29,5 +72,15 @@ number_pages = 50
 
 for page in range(number_pages):
     urls_books.extend(urls_extract(soup_recover(url_web + 'catalogue/page-' + str(page+1) + '.html')))
+    clearConsole()
+    print(str(round((page)*2,1)) + '%')
 
-print (len(urls_books))
+
+data_books = []
+
+for index,url in enumerate(urls_books):
+    data_books.append(extract_data(soup_recover(url)))
+    # clearConsole()
+    print(str(round(index*0.1,1)) + '%')
+
+export_csv(data_books)
